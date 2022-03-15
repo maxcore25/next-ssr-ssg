1392c30c315f471995a5d14e0437d18c4e1e67b8
maxcore25 <mkorobov2002@mail.ru>
2022-03-15 09:45:15 +0300

    Current Pokemon Page Stylized

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,12 +1,16 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../../styles/Details.module.css';
 
 const Detail = () => {
   const {
     query: { id },
   } = useRouter();
 
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
     async function getPokemon() {
@@ -21,7 +25,45 @@ const Detail = () => {
 
   if (!pokemon) return null;
 
-  return <div>{JSON.stringify(pokemon)}</div>;
+  return (
+    <>
+      <Head>
+        <title>{pokemon.name}</title>
+      </Head>
+      <Link href='/'>
+        <a>Back to Home</a>
+      </Link>
+      <div className={styles.layout}>
+        <Image
+          className={styles.picture}
+          src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+          alt='Pokemon'
+          width={400}
+          height={400}
+        />
+      </div>
+      <div>
+        <div className={styles.name}>{pokemon.name}</div>
+        <div className={styles.type}>{pokemon.type.join(', ')}</div>
+        <table>
+          <thead className={styles.header}>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon.stats.map(({ name, value }) => (
+              <tr key={name}>
+                <td className={styles.attribute}>{name}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Detail;
